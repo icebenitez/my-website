67e232b13703bc43d214486453358dfbf99c58de
Ice\UNA <icebenitez@gmail.com>
2026-08-23 01:26:06 +0800

    feat: generate sitemap modification dates

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -1,13 +1,22 @@
+import { statSync } from 'node:fs';
+import { resolve } from 'node:path';
+
 const baseUrl = 'https://icebenitez.com';
+
+function getLastModified(sourceFile: string) {
+  return statSync(resolve(process.cwd(), 'src/pages', sourceFile))
+    .mtime.toISOString()
+    .split('T')[0];
+}
 
 export function GET() {
   const urls = [
-    { path: '/', lastModified: '2025-08-24' },
-    { path: '/privacy-policy/', lastModified: '2025-04-22' },
-    { path: '/terms-of-service/', lastModified: '2025-04-22' },
+    { path: '/', sourceFile: './index.astro' },
+    { path: '/privacy-policy/', sourceFile: './privacy-policy/index.astro' },
+    { path: '/terms-of-service/', sourceFile: './terms-of-service/index.astro' },
   ];
   const body = urls
-    .map(({ path, lastModified }) => `  <url><loc>${baseUrl}${path}</loc><lastmod>${lastModified}</lastmod></url>`)
+    .map(({ path, sourceFile }) => `  <url><loc>${baseUrl}${path}</loc><lastmod>${getLastModified(sourceFile)}</lastmod></url>`)
     .join('\n');
 
   return new Response(`<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n${body}\n</urlset>`, {
